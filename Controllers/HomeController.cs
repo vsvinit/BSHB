9123281cb84e91c9dc07c285dfc034c0ad34322e
Vinit Sinha <vsvinit12@gmail.com>
2024-05-20 11:39:45 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using BiharStateHousingBoard.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BiharStateHousingBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult HomePopUP()
+        {
+            return PartialView("_HomePopUP");
+        }
+        //public PartialViewResult Index()
+        //{
+        //    //return View
+        //    return PartialView("_HomePopUP");
+
+        //}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BiharStateHousingBoard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
